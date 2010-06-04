500f7f665e42675c190cf6297e605fd2b8017228
Jorge Eduardo Cardona Gaviria <jorgeecardona@gmail.com>
2010-06-03 20:33:01 -0500

    recode tokens

--- tests.py
+++ tests.py
@@ -1,5 +1,5 @@
 import unittest
-from tokens import Ident, String, Integer, Number
+from tokens import IDENT, QIDENT, STRING, UNSIGNED_INTEGER, UNSIGNED_NUMBER
 from base import IncorrectValue, ParseError
 
 
@@ -7,64 +7,74 @@ class testTokensLoad(unittest.TestCase):
     def setUp(self):
         pass
 
-    def testIdent(self):
-        self.assertTrue(Ident.load("algo").value == "algo")
-        self.assertTrue(Ident.load("Algo").value == "Algo")
-        self.assertTrue(Ident.load("_algo").value == "_algo")
-        self.assertTrue(Ident.load("  _algo  ").value == "_algo")
-        self.assertTrue(Ident.load("  'algo'  ").value == "'algo'")
+    def test_ident(self):
+        self.assertTrue(IDENT.load("algo").value == "algo")
+        self.assertTrue(IDENT.load("Algo").value == "Algo")
+        self.assertTrue(IDENT.load("_algo").value == "_algo")
+        self.assertTrue(IDENT.load("  _algo  ").value == "_algo")
+        self.assertTrue(IDENT.load("  'algo'  ").value == "'algo'")
 
-        self.assertRaises(ParseError, lambda: Ident.load("1algo"))
-
-
-    def testQIdent(self):
-        self.assertTrue(Ident.load("'algoas'").value == "'algoas'")
+        self.assertRaises(ParseError, lambda: IDENT.load("1algo"))
 
 
-    def testString(self):
-        self.assertTrue(String.load(""" "algo" """).value == "algo")
-        self.assertTrue(String.load(""" "algo y algo" """).value == "algo y algo")
-        self.assertTrue(String.load(""" "algo es \\"algo\\"" """).value == '''algo es "algo"''')
+    def test_qident(self):
+        self.assertTrue(IDENT.load("'algoas'").value == "'algoas'")
+
+
+    def test_string(self):
+        self.assertTrue(STRING.load(""" "algo" """).value == "algo")
+        self.assertTrue(STRING.load(""" "algo y algo" """).value == "algo y algo")
+        self.assertTrue(STRING.load(""" "algo es \\"algo\\"" """).value == '''algo es "algo"''')
         
-    def testInteger(self):
-        self.assertTrue(Integer.load(" 123     ").value == 123)
+    def test_integer(self):
+        self.assertTrue(UNSIGNED_INTEGER.load(" 123     ").value == 123)
 
 
-    def testNumber(self):
-        self.assertEqual(Number.load("  12e-12").dump(), "12e-12")
-        self.assertEqual(Number.load("  12E+12").dump(), "12e12")
+    def test_number(self):
+        self.assertEqual(UNSIGNED_NUMBER.load("  12e-12").dump(), "12e-12")
+        self.assertEqual(UNSIGNED_NUMBER.load("  12E+12").dump(), "12e12")
 
 
-    def testExpression(self):
+    def test_expression(self):
         pass
 
-    def testEquation(self):
+    def test_equation(self):
         pass
 
 class testTokens(unittest.TestCase):
     def setUp(self):
         pass
 
-    def testIdent(self):
+    def test_ident(self):
 
-        self.assertTrue(isinstance(Ident("algo"), Ident))
-        self.assertTrue(isinstance(Ident("_algo"), Ident))
-        self.assertTrue(isinstance(Ident("Algo"), Ident))
+        self.assertTrue(isinstance(IDENT("algo"), IDENT))
+        self.assertTrue(isinstance(IDENT("_algo"), IDENT))
+        self.assertTrue(isinstance(IDENT("Algo"), IDENT))
 
-        self.assertRaises(Exception, lambda: Ident(" algo asi "))
+        self.assertRaises(Exception, lambda: IDENT(" algo asi "))
+
+    def test_qident(self):
+
+        self.assertTrue(isinstance(QIDENT("'algo'"), QIDENT))
+        self.assertTrue(isinstance(QIDENT("'_algo'"), QIDENT))
+        self.assertTrue(isinstance(QIDENT("'Algo'"), QIDENT))
+
+        self.assertRaises(Exception, lambda: QIDENT("' algo asi "))
+
+
 
 class testTokensDumps(unittest.TestCase):
     def setUp(self):
         pass
     
-    def testIdent(self):
-        self.assertEqual(Ident("algo").dump(), "algo") 
+    def test_ident(self):
+        self.assertEqual(IDENT("algo").dump(), "algo") 
 
-    def testString(self):
-        self.assertEqual(String("algo y algo").dump(), '''"algo y algo"''') 
+    def test_string(self):
+        self.assertEqual(STRING("algo y algo").dump(), '''"algo y algo"''') 
 
-    def testInteger(self):
-        self.assertEqual(Integer("12").dump(), '''12''') 
+    def test_integer(self):
+        self.assertEqual(UNSIGNED_INTEGER("12").dump(), '''12''') 
 
         
 
--- tokens.py
+++ tokens.py
@@ -1,140 +1,208 @@
-from pyparsing import Word, nums, Literal, alphas, alphanums, QuotedString, Group
-from base import BaseModelica, IncorrectValue, NonImplemented
+#from pyparsing import Word, nums, Literal, alphas, alphanums, QuotedString, Group
+#from base import BaseModelica, IncorrectValue, NonImplemented
 
-from pyparsing import CharsNotIn, Combine, OneOrMore, ZeroOrMore, Optional, Suppress
-from pyparsing import nums, alphas, alphanums, printables
+#from pyparsing import CharsNotIn, Combine, OneOrMore, ZeroOrMore, Optional, Suppress
+#from pyparsing import nums, alphas, alphanums, printables
 
-from string import printable
+#from string import printable
 
-from decimal import Decimal
+#from decimal import Decimal
 
-s_escape = Literal("\\'") ^ "\\\"" ^ "\\?" ^ "\\\\" ^ "\\a" ^ "\\b" ^ "\\f" ^ "\\n" ^ "\\r" ^ "\\t" ^ "\\v"
+#s_escape = Literal("\\'") ^ "\\\"" ^ "\\?" ^ "\\\\" ^ "\\a" ^ "\\b" ^ "\\f" ^ "\\n" ^ "\\r" ^ "\\t" ^ "\\v"
 
-def unescape(string):
-    return string\
-        .replace("\\\"","\"")\
-        .replace("\\'","'")\
-        .replace("\\\\", "\\")\
-        .replace("\\r", chr(13))\
-        .replace("\\n", chr(10))\
-        .replace("\\t", chr(9))
+# def unescape(string):
+#     return string\
+#         .replace("\\\"","\"")\
+#         .replace("\\'","'")\
+#         .replace("\\\\", "\\")\
+#         .replace("\\r", chr(13))\
+#         .replace("\\n", chr(10))\
+#         .replace("\\t", chr(9))
 
-def escape(string):
-    return string\
-        .replace("\"","\\\"")\
-        .replace("'","\\'")\
-        .replace("\\","\\\\")\
-        .replace(chr(13),"\\r")\
-        .replace(chr(10),"\\n")\
-        .replace(chr(9),"\\t")
+# def escape(string):
+#     return string\
+#         .replace("\"","\\\"")\
+#         .replace("'","\\'")\
+#         .replace("\\","\\\\")\
+#         .replace(chr(13),"\\r")\
+#         .replace(chr(10),"\\n")\
+#         .replace(chr(9),"\\t")
 
-class QIdent(BaseModelica):
+# class QIdent(BaseModelica):
 
-    def __init__(self, value= None):
-        if type(value) is str:
-            self.value = value
-        else:
-            raise IncorrectValue("Non-string passed.")
+#     def __init__(self, value= None):
+#         if type(value) is str:
+#             self.value = value
+#         else:
+#             raise IncorrectValue("Non-string passed.")
 
-    def dump(self):
-        return "'" + self.value + "'"
+#     def dump(self):
+#         return "'" + self.value + "'"
 
-class Ident(BaseModelica):
+# class Ident(BaseModelica):
+#     def __init__(self, value=None):
+#         if isinstance(value, (str, QIdent)):
+#             self.value = str(value)
+#         else:
+#             raise Exception("Non-string or QIdent passed.")
 
-    def __init__(self, value = None):
+#     def dump(self, indent = 0):
+#         return str(self.value)    
 
-        if type(value) is str:
-            self.value = value
-
-        elif type(value) is QIdent:
-            self.value = value.dump()
-
-        else:
-            raise IncorrectValue("Non-string or QIdent passed.")
-
-    def dump(self, indent = 0):
-        return str(self.value)    
-
-    def __repr__(self):
-        return "IDENT: %s"%str(self)
+#     def __repr__(self):
+#         return "IDENT: %s"%str(self)
 
 
-class String(BaseModelica):
-
-    def __init__(self, value= None):
-        # Receive a string with the escaping elements.
+# class String(BaseModelica):
+#     def __init__(self, value=None):
+#         # Receive a string with the escaping elements.
         
-        if type(value) is str:
-            self.value = value
-        else:
-            raise IncorrectValue("Non-string passed.")
+#         if type(value) is str:
+#             self.value = value
+#         else:
+#             raise IncorrectValue("Non-string passed.")
 
-    def dump(self):
-        return '"' + escape(self.value) + '"'
+#     def dump(self):
+#         return '"' + escape(self.value) + '"'
                 
-class Integer(BaseModelica):
-    __ebnf__ = Word(nums).setParseAction(lambda s, l, t: Integer(t[0]))
+# class Integer(BaseModelica):
+#     __ebnf__ = Word(nums).setParseAction(lambda s, l, t: Integer(t[0]))
     
-    def __init__(self, value=0):
-        if type(value) is str:
-            self.value = Decimal(value)
-        else:
-            raise IncorrectValue("Non-string passed.'")
+#     def __init__(self, value=0):
+#         if type(value) is str:
+#             self.value = Decimal(value)
+#         else:
+#             raise IncorrectValue("Non-string passed.'")
     
+#     def dump(self):
+#         return str(self.value)
+
+#     def __repr__(self):
+#         return "INTEGER: %s"%str(self)
+
+# class Number(BaseModelica):
+
+#     mantissa = Decimal(0)
+#     exponent = Decimal(0)
+
+#     def __init__(self, mantissa="0", exponent_sign = "+", exponent = "0"):
+
+#         self.mantissa = Decimal(mantissa)
+#         self.exponent = Decimal(exponent)
+
+#         if exponent_sign is "-":
+#             self.exponent = -self.exponent
+
+
+#     def dump(self):
+#         return str(self.mantissa) + (("e" + str(self.exponent)) if self.exponent !=Decimal(0) else "")
+
+
+#     def __repr__(self):
+#         return "NUMBER: %s"%str(self)
+
+
+# # EBNF Syntax here.
+
+# # IDENT Syntax and action
+# Ident.ebnf(
+#     syntax = Word(alphas + "_", alphanums + "_") ^ QIdent.ebnf(),
+
+#     action = lambda s, l, t: Ident(t[0])
+#     )
+
+# # QIDENT Syntax and action
+# QIdent.ebnf(
+#     syntax = (
+#         Suppress("'") + 
+#         Combine(OneOrMore(CharsNotIn("\\'") ^ s_escape )) + 
+#         Suppress("'")
+#         ),
+
+#     action = lambda s, l, t: QIdent(t[0])
+#     )
+
+# String.ebnf(
+#     syntax = (
+#         Suppress('"') + 
+#         Combine(ZeroOrMore(CharsNotIn("\\\"") ^ s_escape )) +
+#         Suppress('"')
+#         ),
+    
+#     action = lambda s, l, t: String(t[0])
+#     )
+
+# Number.ebnf(
+#     syntax = (
+#         Combine(Word(nums) + Optional(Literal(".") + Optional(Word(nums)))).setResultsName('mantissa') + 
+#         Optional((Literal("e") ^ Literal("E")) + Optional(Literal("+") ^ Literal("-")).setResultsName('exponent_sign') + Word(nums).setResultsName('exponent'))
+#         ).setParseAction(lambda s,l,t: Number(**dict(t)))
+
+#     syntax = 
+
+from pyparsing import Word, nums, alphas, Suppress, OneOrMore, CharsNotIn
+from base import BaseModelica as ModelicaBase
+
+class IDENT(ModelicaBase):
+    def __init__(self, value):
+        if isinstance(value, (unicode, str)):
+            if value == value.strip():
+                self.value = unicode(value)
+                return
+            
+        raise Exception("Non string value")
+
     def dump(self):
-        return str(self.value)
+        return "%s" % (self.value)
 
-    def __repr__(self):
-        return "INTEGER: %s"%str(self)
+class QIDENT(ModelicaBase):
+    def __init__(self, value):
+        if isinstance(value, (unicode, str)):
+            if (value[0] == value[-1] == "'"):
+                self.value = unicode(value[1:-1])
+                return
 
-class Number(BaseModelica):
-    __ebnf__ = (
-        Combine(Word(nums) + Optional(Literal(".") + Optional(Word(nums)))).setResultsName('mantissa') + 
-        Optional((Literal("e") ^ Literal("E")) + Optional(Literal("+") ^ Literal("-")).setResultsName('exponent_sign') + Word(nums).setResultsName('exponent'))
-        ).setParseAction(lambda s,l,t: Number(**dict(t)))
-
-    mantissa = Decimal(0)
-    exponent = Decimal(0)
-
-    def __init__(self, mantissa="0", exponent_sign = "+", exponent = "0"):
-
-        self.mantissa = Decimal(mantissa)
-        self.exponent = Decimal(exponent)
-
-        if exponent_sign is "-":
-            self.exponent = -self.exponent
-
+        raise Exception("Non string value")
 
     def dump(self):
-        return str(self.mantissa) + (("e" + str(self.exponent)) if self.exponent !=Decimal(0) else "")
+        return "'%s'" % (self.value)
+            
 
+class NONDIGIT:
+    pass
 
-    def __repr__(self):
-        return "NUMBER: %s"%str(self)
+class STRING:
+    pass
 
+class SCHAR:
+    pass
 
-Ident.ebnf(
-    syntax = Word(alphas + "_", alphanums + "_") ^ QIdent.ebnf(),
+class QCHAR:
+    pass
 
-    action = lambda s, l, t: Ident(t[0])
+class SESCAPE:
+    pass
+
+class DIGIT:
+    pass
+
+class UNSIGNED_INTEGER:
+    pass
+
+class UNSIGNED_NUMBER:
+    pass
+
+NONDIGIT = alphas + '_'
+DIGIT = nums
+
+IDENT.ebnf(
+    syntax=Word(NONDIGIT , DIGIT + NONDIGIT) ^ QIDENT.ebnf(),
+    action=lambda s,l,t: IDENT(t[0])
     )
 
-QIdent.ebnf(
-    syntax = (
-        Suppress("'") + 
-        Combine(OneOrMore(CharsNotIn("\\'") ^ s_escape )) + 
-        Suppress("'")
-        ),
-
-    action = lambda s, l, t: QIdent(t[0])
+QIDENT.ebnf(
+    syntax=Suppress("'") + OneOrMore(CharsNotIn('\\"')) + Suppress("'"),
+    action=lambda s,l,t: QIDENT(t[0])
     )
 
-String.ebnf(
-    syntax = (
-        Suppress('"') + 
-        Combine(ZeroOrMore(CharsNotIn("\\\"") ^ s_escape )) +
-        Suppress('"')
-        ),
-    
-    action = lambda s, l, t: String(t[0])
-    )
+# EBNF Syntax and action
